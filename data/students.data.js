92bbe900b85080b903225af58f1aa406f30d8449
M-Robledo66 <miguelrobledo773@gmail.com>
2023-01-04 08:54:30 -0600

    express

---
+++ data/students.data.js
@@ -0,0 +1,15 @@
+const students = [
+    {text: 'Good student 1', done: true, _id: 125223},
+    {text: 'Good student 2', done: true, _id: 127904},
+    {text: 'Lazy student 1', done: false, _id: 139608},
+    {text: 'Good student 3', done: true, _id: 125224},
+    {text: 'Good student 4', done: true, _id: 127904},
+    {text: 'Lazy student 2', done: false, _id: 139604},
+    {text: 'Good student 5', done: true, _id: 122223},
+    {text: 'Good student 6', done: true, _id: 127604},
+    {text: 'Lazy student 3', done: false, _id: 139408},
+  ]
+  
+  export { 
+      students
+  }
